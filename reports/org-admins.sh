3cdac8da61e05ef7be46a3ba50485417d77cc38e
Lars Schneider <larsxschneider@gmail.com>
2017-03-16 13:46:21 +0800

    add script to print all admins of an org

---
+++ reports/org-admins.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# Print admins of an org. The GH API allows to query the admins, too,
+# but you would need to be an admin in that org yourself to see
+# non-public members.
+#
+# c.f. https://developer.github.com/v3/orgs/members/
+#
+# Usage:
+#   org-admins.sh [OPTIONS] <org>
+#
+# Options:
+#   -e, --email            Print email addresses instead of usernames.
+#   -n                     Dry-run; only show what would be done.
+#   -h, --help             Display this message.
+#
+# Example:
+#   org-admins.sh testorg
+#
+
+BASE_DIR=$(cd "${0%/*}/.." && pwd)
+. "$BASE_DIR/lib-ghe.sh"
+
+PROPERTY="login"
+while [ $# -gt 0 ]; do
+    case $1 in
+        (-e|--email) PROPERTY="email"; shift;;
+        (-n) DRY_RUN=1; shift;;
+        (-h|--help) usage 2>&1;;
+        (--) shift; break;;
+        (-*) usage "$1: unknown option";;
+        (*) break;;
+    esac
+done
+
+ORG=$1
+if [ -z "$ORG" ]; then
+    usage "Org must not be empty."
+fi
+
+execute << EOF
+    github-env bin/runner -e production "'
+        admins=(User / \"$ORG\").admins.map { |u| u.$PROPERTY }.sort;
+        puts admins.join(\"\\n\");
+    '"
+EOF
