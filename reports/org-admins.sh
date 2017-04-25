f86742a2fe23556fec822889c6c6a7dbdb8eb320
Lars Schneider <larsxschneider@gmail.com>
2017-04-25 12:49:41 +0200

    reports/org-admins: concat email addresses with a ";"

--- reports/org-admins.sh
+++ reports/org-admins.sh
@@ -22,9 +22,10 @@ BASE_DIR=$(cd "${0%/*}/.." && pwd)
 . "$BASE_DIR/lib-ghe.sh"
 
 PROPERTY="login"
+SEPARATOR="\\n"
 while [ $# -gt 0 ]; do
     case $1 in
-        (-e|--email) PROPERTY="email"; shift;;
+        (-e|--email) PROPERTY="email"; SEPARATOR="; "; shift;;
         (-n) DRY_RUN=1; shift;;
         (-h|--help) usage 2>&1;;
         (--) shift; break;;
@@ -40,7 +41,7 @@ fi
 
 execute << EOF
     github-env bin/runner -e production "'
-        puts admins.join(\"\\n\");
         admins=(User / \"$ORG\").admins.select { |u| not u.disabled and not u.suspended_at }.map { |u| u.$PROPERTY }.sort;
+        puts admins.join(\"$SEPARATOR\");
     '"
 EOF
