d56eb7200dd5b13cbaeef293154d069a63681c3e
Lars Schneider <larsxschneider@gmail.com>
2017-09-15 14:34:36 +0200

    git-version: print git versions per user; support SSH protocol

--- reports/git-versions.sh
+++ reports/git-versions.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #
-# This script prints the Git client versions sorted by the number of IP
-# addresses that have used this version recently.
+# Prints the recently used Git client versions per IP address or per user.
 #
 # Usage:
 #   git-versions.sh
 #
 # Options:
+#   -u, --user    Print results per user account (default: IP address)
 #   -a, --all     Process all available logs (rolled logs
 #   -n            Dry-run; only show what would be done.
 #   -h, --help    Display this message.
@@ -20,6 +20,7 @@ BASE_DIR=$(cd "${0%/*}/.." && pwd)
 
 while [ $# -gt 0 ]; do
     case $1 in
+        (-u|--user)     PER_USER=1; shift;;
         (-a|--all)      all_logs; shift;;
         (-h|--help)     usage 2>&1;;
         (-n)            DRY_RUN=1; shift;;
@@ -29,13 +30,38 @@ while [ $# -gt 0 ]; do
     esac
 done
 
-execute << EOF
-    zgrep -hF '||git/' /var/log/haproxy.$LOG |
-        perl -lape 's/.* (.*):.* \[.*\|\|git\/(\d+(?:\.\d+){0,2}).*/\$1 \$2/' |
-        sort |
-        uniq |
-        perl -lape 's/[^ ]+ //' |
-        sort -r -V |
-        uniq -c |
-        awk '{printf("%s\t%s\n",\$2,\$1)}'
+if [ -z $PER_USER ]; then
+
+    # We gather the Git agent via from two different sources here:
+    #  - audit.log:   Currently, the audit log only reports the Git version
+    #                 for "fetch" operations. Clients using the HTTPS and SSH
+    #                 protocol are supported.
+    #  - haproxy.log: This log reports the Git version for all kinds of
+    #                 operations but only for clients that use the HTTPS
+    #                 protocol.
+    execute << EOF
+        {
+            zcat -f /var/log/github/audit.$LOG |
+            perl -ne 'print if s/.*"real_ip":"([^"]*)".*agent=git\/(\d+(?:\.\d+){0,2}).*/\1 \2/' |
+            sort |
+            uniq &
+            zcat -f /var/log/haproxy.$LOG |
+            perl -ne 'print if s/.* (.*):.* \[.*\|\|git\/(\d+(?:\.\d+){0,2}).*/\1 \2/' |
+            sort |
+            uniq;
+        } |
+            sort |
+            uniq |
+            perl -lape 's/[^ ]+ //' |
+            sort -r -V |
+            uniq -c |
+            awk '{printf("%s\t%s\n",\$2,\$1)}'
 EOF
+else
+    execute << EOF
+        zcat -f /var/log/github/audit.$LOG |
+            perl -ne 'print if s/.*"real_ip":"([^"]*)".*"user_login":"([^"]*)".*agent=git\/(\d+(?:\.\d+){0,2}).*/\3 \2 \1/' |
+            sort -r -V |
+            uniq
+EOF
+fi
